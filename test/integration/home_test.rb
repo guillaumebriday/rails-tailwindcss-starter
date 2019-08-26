f8107a780597bb23f99d1cd93029bbf2917a230a
Guillaume Briday <guillaumebriday@gmail.com>
2019-08-26 19:16:13 +0200

    Upgrading to rails 6

--- test/integration/home_test.rb
+++ test/integration/home_test.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class HomeTest < ActionDispatch::IntegrationTest
-  test "the truth" do
+  test 'the truth' do
     assert true
   end
 
-  test "can see the landing page" do
-    get "/"
-    assert_select "h1", "Getting things done! That's what matters!"
+  test 'can see the landing page' do
+    get '/'
+    assert_select 'h1', "Getting things done! That's what matters!"
   end
 end
